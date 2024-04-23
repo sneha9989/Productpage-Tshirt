860a29bf047d4607f0ea722d0bd3626587d810fe
sneha9989 <2005b2.sneha@gmail.com>
2024-04-23 11:46:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeColor(color) {
+    var tshirtImage = document.getElementById('tshirt-image');
+    tshirtImage.src = color + '-tshirt.jpg';
+}
+function buyNow() {
+    alert("Thank you for your purchase!");
+}
